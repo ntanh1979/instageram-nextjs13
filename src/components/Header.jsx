ef598f285dfb52b7e55eba424e3ca6dafa260d2d
ntanh1979 <ntanh1979@gmail.com>
2023-10-20 09:38:29 +0700

    Add search bar to the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,55 @@
 import Image from 'next/image'
 import React from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 export default function Header() {
   return (
-    <div>
-       {/* Left */}
+   
+      
        <div className="flex items-center justify-between max-w-6xl">
-            <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
-                <Image 
-                    src={'/Instagram_logo.png'} 
-                    alt='logo'
-                    layout='fill'
-                    className='object-contain'
-                     
-                />
+         {/* Left */}
+            <div className="flex flex-row ">
+                    <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
+                        <Image 
+                            src={'/Instagram_logo.png'} 
+                            alt='logo'
+                            layout='fill'
+                            className='object-contain'
+                            
+                        />
+                    </div>
+                    <div className="h-24 w-10 relative  lg:hidden cursor-pointer">
+                        <Image 
+                            src={'/Instagram_logo_mobile.svg'} 
+                            alt='logo'
+                            layout='fill'
+                            className='object-contain'
+                            
+                        />
+                    </div>  
+            </div> 
+
+            {/* Middle */}   
+            <div className="relative mt-1  sm:w-7/12 items-center">
+                <div className="absolute mt-2 ml-2 pl-2">
+                    <AiOutlineSearch className='text-xl  text-gray-500 cursor-pointer' />                    
+                 </div>
+                 <input type="text"  className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-offset-blue-700
+                  focus:border-gray-100  rounded-full sm:w-11/12  ' placeholder='Search'/>
             </div>
-            <div className="h-24 w-10 relative  lg:hidden cursor-pointer">
-                <Image 
-                    src={'/Instagram_logo_mobile.svg'} 
-                    alt='logo'
-                    layout='fill'
-                    className='object-contain'
-                     
-                />
-            </div>      
-            <h1>Right Side</h1>
+
+             {/* Right */}
+             <div className="">
+             <h1 className=''>Right Side</h1>
+             </div>
+             
        </div>
 
-       {/* Middle */}
+    
        
 
-       {/* Right */}
+      
 
-    </div>
+     
   )
 }
