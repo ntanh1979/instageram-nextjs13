766b7cb8820d0f655eb93fcb61c1c22d86136552
ntanh1979 <ntanh1979@gmail.com>
2023-10-23 18:04:39 +0700

    Implement sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,54 +12,47 @@ export default function Header() {
     setSearchTerm("Click search")
   }
   return (
-   
-        // <div className="flex mx-4 items-center justify-between  w-full pl-0 sm:pl-[5%] sm:pr-[5%] md:pl-[10%] md:pr-[10%] lg:pl-[20%] :pr-[20%]">
-        <div className="flex mx-4 xl:mx-auto items-center justify-between  max-w-6xl ">
-         {/* Left */}
-            <div className="flex flex-row ml-2 ">
-                    <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
-                        <Image 
-                            src={'/Instagram_logo.png'} 
-                            alt='logo'
-                            layout='fill'
-                            className='object-contain'
-                            
-                        />
-                    </div>
-                    <div className="h-24 w-10 relative  lg:hidden cursor-pointer">
-                        <Image 
-                            src={'/Instagram_logo_mobile.svg'} 
-                            alt='logo'
-                            layout='fill'
-                            className='object-contain'
-                            
-                        />
-                    </div>  
-            </div> 
+            <div className="shadow-sm border-b sticky top-0 bg-white z-30">
 
-            {/* Middle */}   
-            {/* <div className="relative mt-1 mx-auto w-8/12 sm:w-6/12 items-center">
-                <div className="absolute mt-2 ml-2 pl-2">
-                    <AiOutlineSearch className='text-xl mr-2  text-gray-500 cursor-pointer' />                    
-                 </div>
-                 <input type="text"  className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-offset-blue-700
-                  focus:border-gray-100  rounded-full w-11/12  ' placeholder='Search'/>
-                 
-            </div> */}
-            <SearchTextBox value={searchTerm} setValue={setSearchTerm} handleClick={handleOnSearch}
-            />
-           
-
-             {/* Right */}
-             <div className="flex flex-row space-x-4 justify-center items-center">
-                <AiFillHome className='hidden md:inline-flex text-xl hover:text-red-600 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
-                <AiOutlinePlusCircle className='text-xl hover:text-blue-600 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
-                <img src='/user_logo.png' className='h-10 w-10 rounded-full  cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
-             </div>
             
-       </div>
+                        <div className="flex mx-4 xl:mx-auto items-center justify-between  max-w-6xl ">
+                        {/* Left */}
+                            <div className="flex flex-row ml-2 ">
+                                    <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
+                                        <Image 
+                                            src={'/Instagram_logo.png'} 
+                                            alt='logo'
+                                            layout='fill'
+                                            className='object-contain'
+                                            
+                                        />
+                                    </div>
+                                    <div className="h-24 w-10 relative  lg:hidden cursor-pointer">
+                                        <Image 
+                                            src={'/Instagram_logo_mobile.svg'} 
+                                            alt='logo'
+                                            layout='fill'
+                                            className='object-contain'
+                                            
+                                        />
+                                    </div>  
+                            </div> 
 
-        
+                            {/* Middle */}   
+                        
+                            <SearchTextBox value={searchTerm} setValue={setSearchTerm} handleClick={handleOnSearch} />
+                        
+
+                            {/* Right */}
+                            <div className="flex flex-row space-x-4 justify-center items-center">
+                                <AiFillHome className='hidden md:inline-flex text-xl hover:text-red-600 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+                                <AiOutlinePlusCircle className='text-xl hover:text-blue-600 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+                                <img src='/user_logo.png' className='h-10 w-10 rounded-full  cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                            </div>
+                            
+                    </div>
+
+            </div>
 
       
 
