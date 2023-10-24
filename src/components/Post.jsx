9416501287af4388b2bc1e426459c81e68a8608f
ntanh1979 <ntanh1979@gmail.com>
2023-10-24 21:09:55 +0700

    45 - Create the buttons section of the post comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {BiDotsHorizontalRounded} from 'react-icons/bi'
+import {AiOutlineHeart} from 'react-icons/ai'
+import {BsBookmark,BsChatDots} from 'react-icons/bs'
 
 export default function Post({id,username,img,userImg,caption}) {
   return (
@@ -8,11 +10,21 @@ export default function Post({id,username,img,userImg,caption}) {
        <div className="flex items-center p-5">
             <img className="h-12 rounded-full object-cover border p-1 mr-3" src={userImg} alt={username} />
             <p className='font-bold flex-1'>{username}</p>
-            <BiDotsHorizontalRounded className='text-md hover:scale-125 hover:text-green-400'/>
+            <BiDotsHorizontalRounded className='btn' size={'1.5rem'}/>
        </div>
 
         {/* Post image */}
         <img className='object-cover w-full' src={img} alt="" />
+
+        {/* Post Buttons */}
+
+        <div className="flex items-center justify-between px-4 pt-4">
+            <div className="flex items-center space-x-4">
+                <AiOutlineHeart className='btn' size={'1.5rem'} />
+                <BsChatDots className='btn' size={'1.5rem'}/>
+            </div>
+            <BsBookmark className='btn' size={'1.5rem'}/>
+        </div>
     </div>
   )
 }
