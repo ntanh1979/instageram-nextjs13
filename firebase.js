edb9ca0621c434d9cc952c82e940839917b3a58a
ntanh1979 <ntanh1979@gmail.com>
2023-11-14 20:40:44 +0700

    57-Create a post and add it to firestore

--- firebase.js
+++ firebase.js
@@ -8,17 +8,17 @@ import {getStorage} from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 //   "AIzaSyBFB00c62dNBGibEAeEpwMQoun_tHY_D_4",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 //    "insta-v4-14649.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 //   "insta-v4-14649",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 //   "insta-v4-14649.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
 //   "863404275151",
-  appId:  process.env.FIREBASE_APP_ID
+  appId:  process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 //   "1:863404275151:web:1088a1f34688eb50918eba"
 };
 
--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -5,10 +5,22 @@ import Modal from 'react-modal'
 import {FiCamera} from 'react-icons/fi'
 import { useRef ,useState} from 'react'
 
+import {addDoc, collection, doc, serverTimestamp, updateDoc} from "@firebase/firestore"
+import {db,storage} from "../../firebase"
+import { useSession } from 'next-auth/react'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
+
 export default function UploadModal() {
   const [open,setOpen] = useRecoilState(modalState)
+
+  const { data: session } = useSession()
+  
   const filePickerRef = useRef(null)
+  const captionRef = useRef(null)
+
   const [selectedFile,setSelectedFile] = useState(null)
+  const [loading,setLoading] = useState(false)
+  
 
   function addImageToPost (event) {
       const reader = new FileReader()
@@ -19,14 +31,44 @@ export default function UploadModal() {
         setSelectedFile(readerEvent.target.result)
       }
       
+  }
+  ///////////////////////////////////////////////////////////////
+  async function uploadPost () {
+    if(loading) return;
+
+    setLoading(true);
+
+    const docRef = await addDoc(collection(db,"posts"),{
+      caption: captionRef.current.value,
+      username : session.user.username,
+      profileImg : session.user.image,
+      timestamp : serverTimestamp(),
+      
+    }) ;
+
+    const imageRef = ref(storage,`posts/${docRef.id}/image`)
+    await uploadString(imageRef, selectedFile,"data_url").then(
+      async(snapshot)=>{
+        const downloadURL = await getDownloadURL(imageRef)
+        await updateDoc(doc(db,"posts",docRef.id),{
+          image: downloadURL
+        })
+      }
+    );
+    
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+
 
   }
+
   return (
     <div>
        {open &&
         (<Modal
           className="max-w-lg w-[90%] p-6
-          absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md
+          absolute top-56 left-[50%] translate-x-[-50%] bg-white border-1 rounded-md shadow-md
           "
           isOpen = {open}
           onRequestClose={()=>{
@@ -57,12 +99,16 @@ export default function UploadModal() {
               <input type="file" hidden ref={filePickerRef} onChange={addImageToPost}/>
 
               <input type="text" maxLength={150} placeholder='Please enter your caption ...'  
-              className='m-4 border-none text-center w-full focus-within:outline-none'/>
+              className='m-4 border-none text-center w-full focus-within:outline-none'
+              ref = {captionRef} />
+
               <button 
-              disabled
-              className='w-full bg-red-600 text-white p-2 shadow-md   hover:brightness-125 
+              onClick={uploadPost}
+              disabled={!selectedFile || loading}
+              className='w-full bg-red-600 text-white p-2 shadow-md  rounded-lg hover:brightness-125 
               disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100' >
-                Upload Post</button>
+                Upload Post
+              </button>
 
           </div>
           
