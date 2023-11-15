f6278107f2fe7ba104f17a8ba8b0b706fb864778
ntanh1979 <ntanh1979@gmail.com>
2023-11-15 11:39:44 +0700

    58-Get the post data from db and show in the feed

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,30 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from './Post'
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore'
+import {db} from '../../firebase'
 
 export default function Posts() {
-    const posts = [
-        {
-            id: "1",
-            username: "ntanh",
-            userImg : `https://i.pravatar.cc/150?img=${Math.ceil(Math.random()*70)}`,
-            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR418zJYjF6GK5cRJj8XChxCQyEhEZDxwa8SGMv1ujAy4qKQZpv90GYbKFP8xCgy7Q9bjg&usqp=CAU" ,
-            caption : "Nice picture"
-        },
-        {
-            id: "2",
-            username: "nhanh",
-            userImg : `https://i.pravatar.cc/150?img=${Math.ceil(Math.random()*70)}`,
-            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTypoEza8O7zxqquAGdHLezPV0_0AhtZpBcWpHETYNqsQ&s" ,
-            caption : "Nice picture"
-        },
-        {
-            id: "3",
-            username: "ndanh",
-            userImg : `https://i.pravatar.cc/150?img=${Math.ceil(Math.random()*70)}`,
-            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHFz4F5NJ6GX0xPLlDzjHfpE5zjaYw_F-xgL_HGPHS&s" ,
-            caption : "Nice picture"
-        },
-    ]
+    const  [posts,setPosts] = useState([])
+
+    useEffect(()=>{
+      const unsubscribe = onSnapshot(
+         query( collection(db,"posts"),orderBy("timestamp","desc")), 
+          (snapshot) =>{
+            setPosts(snapshot.docs)
+          }
+      )
+      return unsubscribe;
+    },[])
+
   return (
     <div>
       {
@@ -32,10 +23,10 @@ export default function Posts() {
              <Post 
                 key={post.id}  
                 id={post.id}  
-                username = {post.username}
-                userImg= {post.userImg}
-                img = {post.img}
-                caption = {post.caption}
+                username = {post.data().username}
+                userImg= {post.data().profileImg}
+                img = {post.data().image}
+                caption = {post.data().caption}
 
              />   
         )
